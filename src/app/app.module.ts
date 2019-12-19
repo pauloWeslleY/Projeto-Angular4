d19826aa19d9acb60eff35195a7f198379519432
Weslley Lima <weslleyspfc13@outlook.com>
2019-12-19 13:11:04 -0200

    projetosAngular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { TopComponent } from './top/top.component'
+import { PainelComponent } from './painel/painel.component'
+import { AttempsComponent } from './attemps/attemps.component'
+import { ProgressComponent } from './progress/progress.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopComponent,
+    PainelComponent,
+    AttempsComponent,
+    ProgressComponent
   ],
   imports: [
     BrowserModule,
